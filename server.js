70e46663ced3caec994bec5427c1767db03ddcb9
NickAssassin666 <84249423+NickAssassin666@users.noreply.github.com>
2022-04-14 01:56:37 +0530

    first commit without contact

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+app.get("/clients", (req, res) => {
+  res.render("clients");
+});
+
+app.get("/career", (req, res) => {
+  res.render("career");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/services/manpower-supplies", (req, res) => {
+  res.render("manpower-supplies");
+});
+
+app.get("/services/rental-equipment-service", (req, res) => {
+  res.render("rental-equipment-service");
+});
+
+app.get("/services/trading", (req, res) => {
+  res.render("trading");
+});
+
+app.get("/services/general-contracting", (req, res) => {
+  res.render("general-contracting");
+});
+
+app.get("/services/transportation", (req, res) => {
+  res.render("transportation");
+});
+
+app.get("/services/office-supplies", (req, res) => {
+  res.render("office-supplies");
+});
+
+app.get("/services/catering-service", (req, res) => {
+  res.render("catering-service");
+});
+
+app.get("/services/information-technology", (req, res) => {
+  res.render("information-technology");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started at port 3000");
+});
